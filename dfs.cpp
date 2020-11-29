ba24d1c77cc7f77281c64dc80c270935712c294a
Treuce <barthbeni2000@gmail.com>
2020-11-29 09:32:21 +0100

    Indexing vertices starts by one,  not 0, just for ELTE IK

--- dfs.cpp
+++ dfs.cpp
@@ -20,6 +20,8 @@ class Graph
 	}
 	void addEdge(int src, int dest)
 	{
+		--src;
+		--dest;
 		adj[src].push_back(dest);
 	}
 };
@@ -81,14 +83,14 @@ void dfs(Graph g)
 int main()
 {
 	Graph g(6);
-	g.addEdge(0,1);
-	g.addEdge(0,2);
 	g.addEdge(1,2);
-	g.addEdge(1,5);
+	g.addEdge(1,3);
 	g.addEdge(2,3);
-	g.addEdge(3,0);
+	g.addEdge(2,6);
 	g.addEdge(3,4);
 	g.addEdge(4,1);
 	g.addEdge(4,5);
+	g.addEdge(5,2);
+	g.addEdge(5,6);
 	dfs(g);
 }
