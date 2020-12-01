5eb42b895d9a24c1301cef35c8237eb54ee7bf63
Stryse <tianarath30@gmail.com>
2020-12-01 14:32:54 +0100

    Slightly different console output

--- floydwarshall.cpp
+++ floydwarshall.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <limits>
-
+#include <string>
 
 #define V 4
 #define INF 9999
+
+template<typename T>
+void printMatrix(const T& M, const std::string& name = "", int k = -1)
+{
+	std::cout << name << ((k != -1) ? std::to_string(k+1) : "")  << std::endl;
+	for (int i = 0; i < M.size(); ++i)
+		{
+			std::cout << "[\t";
+			for (int j = 0; j < M.size(); ++j)
+			{
+				if (M[i][j] != INF)
+					std::cout << M[i][j] << "\t";
+				else 
+					std::cout << "INF\t";
+			}
+			std::cout << "]" << std::endl;
+		}
+	std::cout << std::endl;
+}
 
 int FloydMarshall(std::vector<std::vector<int>> A)
 // int FloydMarshall(int A[][V])
@@ -12,6 +31,8 @@ int FloydMarshall(std::vector<std::vector<int>> A)
 	int n = A.size();
 	std::vector<std::vector<int>> D(V,std::vector<int>(V));
 	std::vector<std::vector<int>> pi(V,std::vector<int>(V));
+
+	//SETUP
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < n; ++j)
@@ -26,29 +47,7 @@ int FloydMarshall(std::vector<std::vector<int>> A)
 		}
 	}
 	
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-			if (D[i][j] != INF)
-				std::cout << "D0" << "[" << i + 1 << "," << j +1<< "]=" << D[i][j] << "\t";
-			else 
-				std::cout << "D0" << "[" << i +1 << "," << j +1<< "]=INF\t";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << std::endl;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-				std::cout << "Pi0" << "[" << i + 1<< "," << j + 1<< "]=" << pi[i][j] << "\t";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << std::endl << std::endl << std::endl;
-	
-	
+	//F-W
 	for (int k = 0; k < n; ++k)
 	{
 		for (int i = 0; i < n; ++i)
@@ -61,69 +60,32 @@ int FloydMarshall(std::vector<std::vector<int>> A)
 					pi[i][j] = pi[k][j];
 					if (i == j && D[i][i] < 0)
 					{
-						std::cout << "Neg cycle" << std::endl;
+						std::cout << "Neg cycle with node: " << i+1 << std::endl;
 						return i;
 					}
 				}
 			}
 		}
-		for (int i = 0; i < n; ++i)
-		{
-			for (int j = 0; j < n; ++j)
-			{
-				if (D[i][j] != INF)
-					std::cout << "D" << k + 1 << "[" << i + 1 << "," << j + 1<< "]=" << D[i][j] << "\t";
-				else 
-					std::cout << "D" << k + 1 << "[" << i + 1<< "," << j + 1<< "]=INF\t";
-			}
-			std::cout << std::endl;
-		}
-		std::cout << std::endl;
-		for (int i = 0; i < n; ++i)
-		{
-			for (int j = 0; j < n; ++j)
-			{
-					std::cout << "Pi" << k + 1 << "[" << i + 1 << "," << j + 1<< "]=" << pi[i][j] << "\t";
-			}
-			std::cout << std::endl;
-		}
+		std::cout << "================== K: " << k+1 << " ==================" << std::endl << std::endl;
+		printMatrix<>(D,"D",k);
+		printMatrix<>(pi,"Pi",k);
 		std::cout << std::endl;
 	}
-	
-	std::cout << std::endl << std::endl << std::endl;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-			if (D[i][j] != INF)
-				std::cout << "D" << "[" << i + 1 << "," << j +1<< "]=" << D[i][j] << "\t";
-			else 
-				std::cout << "D" << "[" << i +1 << "," << j +1<< "]=INF\t";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << std::endl;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-				std::cout << "Pi" << "[" << i + 1 << "," << j + 1 << "]=" << pi[i][j] << "\t";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << std::endl;
+	std::cout << "============= FINISH RESULT ==============" << std::endl << std::endl;
+	printMatrix<>(D,"D");
+	printMatrix<>(pi,"Pi");
 	
 	return -1;
 }
   
 
 int main()
-{
-    std::vector<std::vector<int>> graph = { {0, 1, INF, 3}, 
-											{INF, 0, INF, 1},  
-											{1, 2,   0, INF}, 
-											{INF, INF, 2, 0} 
-											};  
+{										   // a    b     c   d
+    std::vector<std::vector<int>> graph = { { 0,   1,   INF, 3  }, //a
+											{ INF, 0,   INF, 1  }, //b
+											{ 1,   2,   0,   INF}, //c
+											{ INF, INF, 2,   0}    //d
+										  };  
 	
     // Print the solution  
     int res = FloydMarshall(graph);  
